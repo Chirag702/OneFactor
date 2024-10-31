80b80300374f06317ef71ab21bbfa29ea5729531
Chirag Agarwal <chiragagrawal629@gmail.com>
2024-10-31 13:52:17 +0530

    changes

--- src/PrivateRoute.js
+++ src/PrivateRoute.js
@@ -57,7 +57,7 @@ const PrivateRoute = ({ children }) => {
             }
         };
 
-        getUserData();
+        getUserVerified();
     }, [navigate, location.pathname]);
 
     if (loading) {
